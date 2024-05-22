75e3cea7f99cbefd7d1f7a3b461aaacda8357f0d
Sally Lin <LS0723000@gmail.com>
2024-05-22 17:33:20 +0800

    Add return type to utils functions

--- utils.py
+++ utils.py
@@ -2,15 +2,15 @@ import numpy as np
 from PIL import Image
 
 
-def encode_message(message: str):
+def encode_message(message: str) -> list[str]:
     return [format(ord(x), '08b') for x in message]
 
 
-def decode_message(message_list: list[str]):
+def decode_message(message_list: list[str]) -> str:
     return "".join([chr(int(x, 2)) for x in message_list])
 
 
-def image_array_reshape(image: Image):
+def image_array_reshape(image: Image) -> tuple[np.array, type(tuple)]:
     pixels = np.array(image)
     shape = pixels.shape
 
@@ -19,5 +19,5 @@ def image_array_reshape(image: Image):
     return pixels, shape
 
 
-def text_fits_in_image(pixels: np.array, text: str):
+def text_fits_in_image(pixels: np.array, text: str) -> bool:
     return len(text) <= len(pixels) // 3
