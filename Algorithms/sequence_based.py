5fcbf6b9fc11454d68858abf77c0426e822658f5
Sally Lin <LS0723000@gmail.com>
2024-05-31 09:23:04 +0800

    Refactor Sequence Steg Feature

--- Algorithms/sequence_based.py
+++ Algorithms/sequence_based.py
@@ -1,50 +1,39 @@
 import numpy as np
 
 
-def modify_pixel(pixels: np.array, bin_messages: str):
+def modify_pixel(pixels: np.array, bin_message: str):
     pos = 0
     tun = 0
 
     # Encode the messages
-    for i in range(len(bin_messages)):
-        if bin_messages[i] == '0':
-            pixels[tun][pos] = pixels[tun][pos] - (1 if pixels[tun][pos] > 0 else -1) if \
-                pixels[tun][pos] % 2 == 1 else pixels[tun][pos]
-        else:
-            pixels[tun][pos] = pixels[tun][pos] - (1 if pixels[tun][pos] > 0 else -1) if \
-                pixels[tun][pos] % 2 == 0 else pixels[tun][pos]
+    for i in range(len(bin_message)):
+        pixels[tun][pos] = (pixels[tun][pos] & 254) | int(bin_message[i])
+
         pos += 1
-        if pos == 3:
-            pos = 0
-            tun += 1
+        tun += pos // 3
+        pos = pos % 3
 
     # Set the end of the messages
     for i in range(8):
-        if pixels[tun][pos] % 2 == 1:
-            pixels[tun][pos] = pixels[tun][pos] - (1 if pixels[tun][pos] > 0 else -1)
-        pos += 1
-        if pos == 3:
-            pos = 0
-            tun += 1
+        pixels[tun][pos] = (pixels[tun][pos] & 254) | 0
 
+        pos += 1
+        tun += pos // 3
+        pos = pos % 3
 
 
 def get_hidden_messages(pixels: np.array):
-    messages = ''
-    pos = 0
-    tun = 0
+    secret = ''
     zero_count = 0
 
     for i in range(len(pixels)):
-        messages += str(pixels[tun][pos] % 2)
-        zero_count = zero_count + 1 if messages[-1] == '0' else 0
-        if zero_count == 8:
-             break
-        pos += 1
-        if pos == 3:
-            pos = 0
-            tun += 1
+        tun = i // 3
+        pos = i % 3
 
-    messages = messages[:-8]
-    messages = [messages[i:i+8] for i in range(0, len(messages), 8)]
-    return messages
+        secret += str(pixels[tun][pos] % 2)
+        zero_count = zero_count + 1 if secret[-1] == '0' else 0
+
+        if zero_count == 8:
+            break
+
+    return [secret[i:i + 8] for i in range(0, len(secret) - 8, 8)]
